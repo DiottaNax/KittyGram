b63da733877f353bcff6e8be5fdce784006ade10
astrobaleno <javid.ameri@studio.unibo.it>
2024-05-02 17:04:48 +0200

    decided to create a specific js file for the script of the Follow/Following button instead of writing it inside the relative html files

---
+++ js/FollowButton.js
@@ -0,0 +1,12 @@
+function toggleFollow() {
+    var button = document.getElementById("followButton");
+    if (button.innerText === "Following") {
+        button.innerText = "Follow";
+        button.classList.remove("btn-light");
+        button.classList.add("btn-primary");
+    } else {
+        button.innerText = "Following";
+        button.classList.remove("btn-primary");
+        button.classList.add("btn-light");
+    }
+}
