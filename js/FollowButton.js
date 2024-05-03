2d212fba0ddc6a2f0192e60c0243c9989c31947a
astrobaleno <javid.ameri@studio.unibo.it>
2024-05-03 17:46:47 +0200

    solved the conflict between of the followbutton in OtherProfile.html with the followbutton from the notification of the navbar

--- js/FollowButton.js
+++ js/FollowButton.js
@@ -10,3 +10,16 @@ function toggleFollow() {
         button.classList.add("btn-light");
     }
 }
+
+function toggleFollowNavbar() {
+    var button = document.getElementById("followButtonNavbar");
+    if (button.innerText === "Following") {
+        button.innerText = "Follow";
+        button.classList.remove("btn-light");
+        button.classList.add("btn-primary");
+    } else {
+        button.innerText = "Following";
+        button.classList.remove("btn-primary");
+        button.classList.add("btn-light");
+    }
+}
