5dfc61da0b9556aef4bd59ba4c6149484e28c271
Nax <federico.diotallevi@studio.unibo.it>
2024-05-08 13:34:02 +0200

    changed local variables' name

--- api/register.php
+++ api/register.php
@@ -1,7 +1,6 @@
 <?php
 
 require_once ("../db/db-config.php");
-require_once ("../db/database.php");
 require_once ("../utils/login_utilities.php");
 
 $result['success'] = false;
@@ -10,19 +9,23 @@ if (isset($_POST['username'], $_POST['email'], $_POST['name'], $_POST['surname']
     // Recupero la password criptata dal form di inserimento.
     $password = $_POST['password'];
     // Crea una chiave casuale
-    $random_salt = hash('sha512', uniqid(mt_rand(1, mt_getrandmax()), true));
+    $salt = hash('sha512', uniqid(mt_rand(1, mt_getrandmax()), true));
     // Crea una password usando la chiave appena creata.
-    $password = hash('sha512', $password . $random_salt);
+    $password = hash('sha512', $password . $salt);
     $username = $_POST['username'];
     $email = $_POST['email'];
     $name = $_POST['name'];
     $surname = $_POST['surname'];
 
     if($dbh->isUsernameTaken($username)) {
-        $register['error'] = "Username already taken! Don't leave us, choose another one :)";
-    } else {
+        $result['error'] = "Username already taken!\n Don't leave us, choose another one :)";
+    } else if($dbh->isEmailTaken($email)){
+        $result['error'] = "Email already taken!\n Don't leave us, choose another one :)";
+    }
+    else {
         $dbh->addAccount($username, $email, $name, $surname, $password, $salt);
         login($username, $password, $dbh);
+        $_SESSION['$username'] = $username;
         $result['success'] = true;
     }
 }
