03bf811842fa2fddcc53f0e491bc99862db6f210
Nax <federico.diotallevi@studio.unibo.it>
2024-05-08 13:32:32 +0200

    added label for signup result

--- signup.php
+++ signup.php
@@ -10,6 +10,10 @@
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 
+<script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
+
+<!-- utility for signup -->
+<script src="./js/signup.js"></script>
 
 <title>Sign Up Page</title>
 </head>
@@ -21,10 +25,10 @@
       <img class="w-100" src="./img/KittyGram_Logo.png" alt="KittyGram logo" >
     </header>
 
-    <form class="text-center align-items-center py-5" action="**your_script.php**" method="post">
+    <form class="text-center align-items-center py-5" id="signupForm">
       <div class="row">
         <div class="col g-3 form-floating">
-          <input type="text" class="form-control" name="Name" id="nameId" placeholder="Name" aria-label="Last name" required>
+          <input type="text" class="form-control" name="name" id="nameId" placeholder="Name" aria-label="Name" required>
           <label for="nameId">Name</label>
         </div>
         <div class="col g-3 form-floating">
@@ -39,7 +43,7 @@
         </div>
         <div class="col g-3 form-floating">
           <input type="password" class="form-control" name="password" id="passwordId" aria-label="Password" placeholder="Password" required>
-          <label for="emailId">Password</label>
+          <label for="passwordId">Password</label>
         </div>
       </div>
       <div class="row">
@@ -51,18 +55,19 @@
         </div>
         <div class="col g-3">
             <div class="form-floating">
-                <input type="text" class="form-control" name="confirmPassword" id="confirmPasswordId" aria-label="Confirm password" placeholder="Confirm password" required>
+                <input type="password" class="form-control" name="confirmPassword" id="confirmPasswordId" aria-label="Confirm password" placeholder="Confirm password" required>
                 <label for="confirmPasswordId">Confirm password</label>
             </div>
-          
         </div>
       </div>
-      <div class="mt-5 justify-content-center">
+
+      <label class="text-center mt-5" id="resultId" name="result"></label>
+
+      <div class="mt-1 justify-content-center">
         <button type="submit" class="btn btn-bd-primary w-100">Signup</button>
       </div>
     </form>
   </div>
-
 
 </body>
 </html>
