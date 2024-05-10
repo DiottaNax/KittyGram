eb73622e5687c6aaafefaa3b2a832983af0c5ac2
Nax <federico.diotallevi@studio.unibo.it>
2024-05-10 19:25:07 +0200

    added axios and some missing id

--- login.php
+++ login.php
@@ -16,6 +16,8 @@
     <!-- Collega il file JavaScript di Bootstrap tramite CDN (è necessario per alcuni componenti di Bootstrap come il carousel, il modal, ecc.) -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
 
+    <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
+
     <script src="./js/login.js"></script>
 
     <title>Login Page</title>
@@ -35,29 +37,32 @@
 
       <form
         class="text-center align-items-center py-4 m-4"
-        action="**your_script.php**"
-        method="post"
+        id="loginFormId"
+        name="loginForm"
       >
         <div class="form-floating mb-3">
           <input
-            type="email"
+            type="text"
             class="form-control"
-            id="floatingInput"
-            placeholder="name@example.com"
+            placeholder="happykitty"
+            id="usernameId"
+            name="username"
           />
-          <label for="floatingInput">Email address</label>
+          <label for="usernameId">Username</label>
         </div>
         <div class="form-floating mb-5">
           <input
             type="password"
             class="form-control"
-            id="floatingPassword"
             placeholder="Password"
+            id="passwordId"
+            name="password"
           />
-          <label for="floatingPassword">Password</label>
+          <label for="passwordId">Password</label>
         </div>
-        <div class="mb-3 justify-content-center">
-          <button type="submit" class="btn btn-bd-primary w-100">Login</button>
+        <label class="text-center" id="resultId" name="result"></label>
+        <div class="mb-3 mt-1 justify-content-center">
+          <button type="submit" id="submitId" class="btn btn-bd-primary w-100">Login</button>
         </div>
         <hr />
         <div class="mt-3 justify-content-center">
