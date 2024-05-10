d7b319726c00c9b047f9762c98df18da9c8ac602
Nax <federico.diotallevi@studio.unibo.it>
2024-05-10 19:31:20 +0200

    added event listener to form

--- js/login.js
+++ js/login.js
@@ -1,5 +1,31 @@
 document.addEventListener("DOMContentLoaded", function () {
-    document.getElementById("signupId").onclick = () => {
-        window.location.href = "./signup.php";
-    };
-})
+  event.preventDefault();
+
+  // Event listener for signup button
+  document.getElementById("signupId").onclick = () => {
+    window.location.href = "./signup.php";
+  };
+
+  // Send axios request to process_login.php
+  document
+    .getElementById("loginFormId")
+    .addEventListener("submit", function () {
+      event.preventDefault();
+      var form = document.getElementById("loginFormId");
+      var formData = new FormData(form);
+
+      formData.append("username", document.getElementById("usernameId").value);
+      formData.append("password", document.getElementById("passwordId").value);
+
+      axios.post("./api/process_login.php", formData).then((response) => {
+          console.log(response.data);
+          document.getElementById("resultId").innerText= response.data["message"];
+        if (response.data["success"]) {
+          document.getElementById("resultId").style.color = "green";
+          setTimeout(() => (window.location.href = "index.php"), 500);
+        } else {
+          document.getElementById("resultId").style.color = "red";
+        }
+      });
+    });
+});
