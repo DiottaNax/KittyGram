75991653e6abd3a7ac5461f6f3b7ce45cf66daf1
Nax <federico.diotallevi@studio.unibo.it>
2024-05-13 18:44:39 +0200

    added redirection to index

--- api/logout.php
+++ api/logout.php
@@ -3,7 +3,6 @@
 require ('../utils/login_utilities.php');
 
 sec_session_start();
-// Elimina tutti i valori della sessione.
 $_SESSION = array();
 // Recupera i parametri di sessione.
 $params = session_get_cookie_params();
@@ -11,4 +10,4 @@ $params = session_get_cookie_params();
 setcookie(session_name(), '', time() - 42000, $params["path"], $params["domain"], $params["secure"], $params["httponly"]);
 // Cancella la sessione.
 session_destroy();
-header('Location: ./');
+header('Location: ../index.php');
