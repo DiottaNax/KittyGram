ed941ee4913aca6adfee7545b7dcc38e175b02c2
Saint138 <93931915+Saint138@users.noreply.github.com>
2024-05-18 13:43:11 +0200

    [DEV] made feed for home

--- index.php
+++ index.php
@@ -3,7 +3,10 @@
 require_once ("db-config.php");
 
 if (isset($_SESSION['user_id'])) {
+    $templateParams["name"] = "show-post-home.php";
+    $templateParams["posts"] = $dbh->getHome($_SESSION["user_id"]);
     require ("base.php");
 } else {
+    $templateParams["title"] = "Login";
     require ("login.php");
 }
