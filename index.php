19fcf3cac7801e3b0501153884e93162d996e216
Nax <federico.diotallevi@studio.unibo.it>
2024-05-23 11:52:23 +0200

    deleted template param posts

--- index.php
+++ index.php
@@ -4,7 +4,6 @@ require_once ("db-config.php");
 
 if (isset($_SESSION['user_id'])) {
     $templateParams["name"] = "show-post-home.php";
-    $templateParams["posts"] = $dbh->getHome($_SESSION["user_id"]);
     require ("base.php");
 } else {
     $templateParams["title"] = "Login";
--- show-post-home.php
+++ show-post-home.php
@@ -1,8 +1,13 @@
 <link rel="stylesheet" href="./css/style.css" />
-<?php if (!empty($templateParams["posts"])): ?>
+
+<?php
+
+$feed  = $dbh->getHome($_SESSION["user_id"]);
+if (!empty($feed)): ?>
+    
     <?php
-    foreach ($templateParams["posts"] as $post):
-        ?>
+    foreach ($feed as $post):
+    ?>
         <article class="article clickable post mb-4 p-4 shadow-sm rounded-5 mt-5 bg-white border border-dark">
             <div class="row">
                 <div class="col text-end me-2">
@@ -41,12 +46,8 @@
                 </div>
                 <!-- Row for comments and likes -->
                     <div class="col text-end mb-2">
-                        <button class="btn comment" type=" button" data-bs-toggle="modal" data-bs-target="#comment-modal"
-                            data-postid=<?php echo $post["post_id"]; ?>>
-                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20"
-                                class=" bi bi-chat" viewBox="0 0 16 16">
-                                <path
-                                    d="M2.678 11.894a1 1 0 0 1 .287.801 11 11 0 0 1-.398 2c1.395-.323 2.247-.697 2.634-.893a1 1 0 0 1 .71-.074A8 8 0 0 0 8 14c3.996 0 7-2.807 7-6s-3.004-6-7-6-7 2.808-7 6c0 1.468.617 2.83 1.678 3.894m-.493 3.905a22 22 0 0 1-.713.129c-.2.032-.352-.176-.273-.362a10 10 0 0 0 .244-.637l.003-.01c.248-.72.45-1.548.524-2.319C.743 11.37 0 9.76 0 8c0-3.866 3.582-7 8-7s8 3.134 8 7-3.582 7-8 7a9 9 0 0 1-2.347-.306c-.52.263-1.639.742-3.468 1.105" />
+                        <button class="btn comment" type=" button" data-bs-toggle="modal" data-bs-target="#comment-modal" data-postid=<?php echo $post["post_id"]; ?>>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" class=" bi bi-chat" viewBox="0 0 16 16"> <path d="M2.678 11.894a1 1 0 0 1 .287.801 11 11 0 0 1-.398 2c1.395-.323 2.247-.697 2.634-.893a1 1 0 0 1 .71-.074A8 8 0 0 0 8 14c3.996 0 7-2.807 7-6s-3.004-6-7-6-7 2.808-7 6c0 1.468.617 2.83 1.678 3.894m-.493 3.905a22 22 0 0 1-.713.129c-.2.032-.352-.176-.273-.362a10 10 0 0 0 .244-.637l.003-.01c.248-.72.45-1.548.524-2.319C.743 11.37 0 9.76 0 8c0-3.866 3.582-7 8-7s8 3.134 8 7-3.582 7-8 7a9 9 0 0 1-2.347-.306c-.52.263-1.639.742-3.468 1.105" />
                             </svg>
                         </button>
 
