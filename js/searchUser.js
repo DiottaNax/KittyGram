62b986c98a56a7daf06fd7b7b754a9eca699395a
Nax <federico.diotallevi@studio.unibo.it>
2024-05-27 18:05:32 +0200

    deleted searchUser function

--- js/searchUser.js
+++ js/searchUser.js
@@ -1,22 +1,19 @@
-function searchUser() {
-  // Effettuare una richiesta GET utilizzando Axios
-  const searchTerm = document.getElementById("searchInput").value;
-  axios
-    .get("./api/search-user.php?searchTerm=" + searchTerm)
-    .then((response) => {
-      // Aggiornare il corpo del modal con i risultati della ricerca
-      document.getElementById("searchModalBody").innerHTML = response.data;
-    })
-    .catch((error) => {
-      console.error("Errore durante la richiesta:", error);
-    });
-}
 
 document.addEventListener("DOMContentLoaded", function () {
   
     document
       .getElementById("searchInput")
       .addEventListener("input", function (event) {
-        searchUser();
+        const searchTerm = document.getElementById("searchInput").value;
+        axios
+          .get("./api/search-user.php?searchTerm=" + searchTerm)
+          .then((response) => {
+            // Aggiornare il corpo del modal con i risultati della ricerca
+            document.getElementById("searchModalBody").innerHTML =
+              response.data;
+          })
+          .catch((error) => {
+            console.error("Errore durante la richiesta:", error);
+          });
       });
 });
