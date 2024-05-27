98c17dce12b440839aba604e64b514a7d6d38095
astrobaleno <javid.ameri@studio.unibo.it>
2024-05-27 22:26:20 +0200

    added the post's date under the description in open-post

--- open-post.php
+++ open-post.php
@@ -60,7 +60,7 @@ if(isset($post)):
                             <p class="mb-0 mt-1"><?php echo $post['description'] ?></p>
                         </div>
                     </div>
-                    <p class="smaller-text mb-0 text-end">Dec 21 1981</p>
+                    <p class="smaller-text mb-0 me-2 text-end"><?php echo $post['date'] ?></p>
                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-three-dots position-absolute top-0 end-0 m-3" data-bs-toggle="modal" data-bs-target="#post-settings-modal" viewBox="0 0 16 16">
                         <path d="M3 9.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3m5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3m5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3"/>
                     </svg>
