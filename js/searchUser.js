35f7bbbbc6033417a7ab14529fe6ac7c1ee58624
Nax <federico.diotallevi@studio.unibo.it>
2024-05-27 18:17:18 +0200

    modal's input reset after closing

--- js/searchUser.js
+++ js/searchUser.js
@@ -1,19 +1,22 @@
-
 document.addEventListener("DOMContentLoaded", function () {
-  
-    document
-      .getElementById("searchInput")
-      .addEventListener("input", function (event) {
-        const searchTerm = document.getElementById("searchInput").value;
-        axios
-          .get("./api/search-user.php?searchTerm=" + searchTerm)
-          .then((response) => {
-            // Aggiornare il corpo del modal con i risultati della ricerca
-            document.getElementById("searchModalBody").innerHTML =
-              response.data;
-          })
-          .catch((error) => {
-            console.error("Errore durante la richiesta:", error);
-          });
-      });
+  document
+    .getElementById("searchInput")
+    .addEventListener("input", function (event) {
+      const searchTerm = document.getElementById("searchInput").value;
+      axios
+        .get("./api/search-user.php?searchTerm=" + searchTerm)
+        .then((response) => {
+          // Aggiornare il corpo del modal con i risultati della ricerca
+          document.getElementById("searchModalBody").innerHTML = response.data;
+        })
+        .catch((error) => {
+          console.error("Errore durante la richiesta:", error);
+        });
+    });
+
+  $("#searchModal").on("hidden.bs.modal", function (e) {
+    // Reimposta il valore dell'input a una stringa vuota
+    $("#searchInput").val("");
+    $("#searchModalBody").innerHTML = "<p></p>";
+  });
 });
