a4407753fbf356b37aefe68c617e80165322b1dc
astrobaleno <javid.ameri@studio.unibo.it>
2024-05-28 18:58:47 +0200

    implemented the navbar for divide the home in posts and adoptions

--- show-post-home.php
+++ show-post-home.php
@@ -22,6 +22,58 @@ if (isset($_GET['route']) && $_GET['route'] == 'adoptions') {
 ?>
 
 <div class="posts-container d-flex flex-column align-items-center justify-content-center">
+    <!-------------- divisione di "POST" e "ADOPTIONS" -------------->
+    <?php
+    // Determina la pagina corrente
+    $current_route = isset($_GET['route']) && ($_GET['route'] == 'posts' || $_GET['route'] == 'adoptions') ? $_GET['route'] : 'posts';
+    ?>
+
+    <!-- Contenitore esterno per centrare la barra indicatore attivo -->
+    <div class=" container-fluid justify-content-center ">
+      <!-- Contenitore della barra indicatore attivo -->
+      <div class="active-indicator-container mt-3">
+        <div class="active-indicator">
+          <div class="half-bar half-bar-left <?php echo $current_route == 'posts' ? 'active' : 'inactive'; ?>"></div>
+          <div class="half-bar half-bar-right <?php echo $current_route == 'adoptions' ? 'active' : 'inactive'; ?>"></div>
+        </div>
+      </div>
+    </div>
+
+
+    <nav class="navbar navbar-expand">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#"></a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup"
+          aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse justify-content-center" id="navbarNavAltMarkup">
+          <div class="navbar-nav mx-auto">
+            <!-- tasto POSTS (con icona relativa) -->
+            <a class="nav-link mx-3 <?php echo $isAdoption ? 'active' : ''; ?>" aria-current="page"
+              href="base.php?>">
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-images"
+                viewBox="0 0 16 16">
+                <path d="M4.502 9a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3" />
+                <path
+                  d="M14.002 13a2 2 0 0 1-2 2h-10a2 2 0 0 1-2-2V5A2 2 0 0 1 2 3a2 2 0 0 1 2-2h10a2 2 0 0 1 2 2v8a2 2 0 0 1-1.998 2M14 2H4a1 1 0 0 0-1 1h9.002a2 2 0 0 1 2 2v7A1 1 0 0 0 15 11V3a1 1 0 0 0-1-1M2.002 4a1 1 0 0 0-1 1v8l2.646-2.354a.5.5 0 0 1 .63-.062l2.66 1.773 3.71-3.71a.5.5 0 0 1 .577-.094l1.777 1.947V5a1 1 0 0 0-1-1z" />
+              </svg> POSTS</a>
+            <!-- tasto ADOPTIONS (con icona relativa) -->
+            <a class="nav-link mx-3 <?php echo $isAdoption ? 'active' : ''; ?>"
+              href="base.php?route=adoptions">
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                class="bi bi-house-heart-fill" viewBox="0 0 16 16">
+                <path
+                  d="M7.293 1.5a1 1 0 0 1 1.414 0L11 3.793V2.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v3.293l2.354 2.353a.5.5 0 0 1-.708.707L8 2.207 1.354 8.853a.5.5 0 1 1-.708-.707z" />
+                <path
+                  d="m14 9.293-6-6-6 6V13.5A1.5 1.5 0 0 0 3.5 15h9a1.5 1.5 0 0 0 1.5-1.5zm-6-.811c1.664-1.673 5.825 1.254 0 5.018-5.825-3.764-1.664-6.691 0-5.018" />
+              </svg> ADOPTIONS</a>
+          </div>
+        </div>
+      </div>
+    </nav>
+
+
     <?php if (!empty($feedToShow)): ?>
         <?php foreach ($feedToShow as $post): ?>
             <article class="article clickable post mb-4 p-4 shadow-sm rounded-5 mt-5 bg-white border border-dark">
