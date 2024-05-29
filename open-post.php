d3839e15c8c0e41a91f80afd3d42c4ef3db84371
astrobaleno <javid.ameri@studio.unibo.it>
2024-05-29 18:02:47 +0200

    fixed post id and owner id on like button

--- open-post.php
+++ open-post.php
@@ -110,8 +110,8 @@ if (isset($currentPost)):
                             $likedClass = $liked ? 'liked' : '';
                         ?>
                         <!-- tasto like -->
-                        <div class="like-icon <?php echo $likedClass ?>" data-post-id="<?php echo $post["post_id"]; ?>"
-                                    data-owner-id="<?php echo $post["user_id"]; ?>">
+                        <div class="like-icon <?php echo $likedClass ?>" data-post-id="<?php echo $currentPost["post_id"]; ?>"
+                                    data-owner-id="<?php echo $currentPost["user_id"]; ?>">
                                     <svg width="25" height="25" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16">
                                         <path
                                             d="<?php echo $likedClass == 'liked' ? 'M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314' : 'm8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143q.09.083.176.171a3 3 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15'; ?>" />
