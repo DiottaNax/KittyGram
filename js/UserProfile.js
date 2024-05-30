6945c2e9d018d2a3ade6938dd697cba9744e49b2
Nax <federico.diotallevi@studio.unibo.it>
2024-05-30 18:17:51 +0200

    removed unused event listeners

--- js/UserProfile.js
+++ js/UserProfile.js
@@ -1,16 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
-  console.log("first enter");
-  const taggedSwitch = document.getElementById("taggedSwitch");
-  const userPostsContainer = document.getElementById("userPostsContainer");
-  const adoptionsContainer = document.getElementById("adoptionsContainer");
   const userSettingsButton = document.getElementById("user-settings-button");
-
-  taggedSwitch.addEventListener("change", function () {
-    if (this.checked) {
-      userPostsContainer.style.display = "none";
-      adoptionsContainer.style.display = "block";
-    }
-  });
 
   if (userSettingsButton != null) {
     userSettingsButton.addEventListener("click", (event) => {
