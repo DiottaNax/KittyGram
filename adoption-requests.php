d67945ed050278074d406bb2a1e43086c7659ecf
Nax <federico.diotallevi@studio.unibo.it>
2024-05-31 20:26:34 +0200

    addded text for no adoption request

--- adoption-requests.php
+++ adoption-requests.php
@@ -8,7 +8,7 @@ if (isset($_GET['post_id'])):
     if ($_SESSION['user_id'] == $adoption['user_id']):
 
         $adoptionRequests = $dbh->getAdoptionRequests($adoption['post_id']);
-    ?>
+        ?>
 
         <!doctype html>
         <html lang="en">
@@ -26,7 +26,7 @@ if (isset($_GET['post_id'])):
                 integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 
             <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
-            
+
             <script src="js/adoptionRequests.js"></script>
 
 
@@ -42,35 +42,40 @@ if (isset($_GET['post_id'])):
                 <h1 class="text-center">Adoption Requests</h1>
             </div>
 
+            <?php if (count($adoptionRequests) == 0): ?>
+                <div class="container-fluid text-center justify-content-center">
+                    <p class="d-flex justify-content-center text-center align-items-center">No adoption request yet 😿!</p>
+                </div>
+            <?php endif; ?>
+
             <?php foreach ($adoptionRequests as $adoptionRequest):
                 $submitter = $dbh->getAccountFromId($adoptionRequest['user_id']); ?>
-            <!-- card di una richiesta di adozione -->
-            <div class="container d-flex justify-content-center align-items-center mb-5">
-                <div class="card text-center">
-                    <!-- icona del cestino -->
-                    <svg
-                        data-post-id="<?php echo $adoptionRequest['post_id'] ?>"
-                        data-submitter="<?php echo $submitter['username'] ?>"
-                        xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
-                        class="remove-request bi bi-trash3 position-absolute top-0 end-0 m-3" viewBox="0 0 16 16">
-                        <path
-                            d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5M11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47M8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5" />
-                    </svg>
-                    <div class="card-body">
-                        <div class="d-flex align-items-center mb-3">
-                            <img src="./img/<?php echo $submitter['pic']; ?>" class="rounded-circle me-2" alt="profile-pic" width="35"
-                                height="35" />
-                            <div class="text-start">
-                                <h5 class="card-title mb-1"><?php echo $submitter['username']; ?></h5>
-                                <p class="card-subtitle text-muted small mb-0"><?php echo $adoptionRequest['cell'] ?></p>
+                <!-- card di una richiesta di adozione -->
+                <div class="container d-flex justify-content-center align-items-center mb-5">
+                    <div class="card text-center">
+                        <!-- icona del cestino -->
+                        <svg data-post-id="<?php echo $adoptionRequest['post_id'] ?>"
+                            data-submitter="<?php echo $submitter['username'] ?>" xmlns="http://www.w3.org/2000/svg" width="16"
+                            height="16" fill="currentColor" class="remove-request bi bi-trash3 position-absolute top-0 end-0 m-3"
+                            viewBox="0 0 16 16">
+                            <path
+                                d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5M11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47M8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5" />
+                        </svg>
+                        <div class="card-body">
+                            <div class="d-flex align-items-center mb-3">
+                                <img src="./img/<?php echo $submitter['pic']; ?>" class="rounded-circle me-2" alt="profile-pic"
+                                    width="35" height="35" />
+                                <div class="text-start">
+                                    <h5 class="card-title mb-1"><?php echo $submitter['username']; ?></h5>
+                                    <p class="card-subtitle text-muted small mb-0"><?php echo $adoptionRequest['cell'] ?></p>
+                                </div>
                             </div>
+                            <p class="card-text">
+                                <?php echo $adoptionRequest['presentation'] ?>
+                            </p>
                         </div>
-                        <p class="card-text">
-                            <?php echo $adoptionRequest['presentation'] ?>
-                        </p>
                     </div>
                 </div>
-            </div>
             <?php endforeach; ?>
         </body>
 
