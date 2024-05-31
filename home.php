4f7b29c29c8c987081032c9ba90bf134c30edd52
astrobaleno <javid.ameri@studio.unibo.it>
2024-05-31 18:28:38 +0200

    added count of number of likes and comments for each post both in home and in open-post pages

--- home.php
+++ home.php
@@ -161,9 +161,11 @@
                       <div class="d-flex align-items-center">
                         <div class="like-icon <?php echo $likedClass ?>" data-post-id="<?php echo $post["post_id"]; ?>"
                           data-owner-id="<?php echo $post["user_id"]; ?>">
+                          <!-- icona like -->
                           <svg width="25" height="25" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16">
                             <path
                               d="<?php echo $likedClass == 'liked' ? 'M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314' : 'm8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143q.09.083.176.171a3 3 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15'; ?>" />
+                              <text x="8" y="8" fill="black" font-size="6" text-anchor="middle" alignment-baseline="middle"><?php echo $dbh->getNumLikes($post['post_id']) ?></text>
                           </svg>
                         </div>
                         <p class="likenumber mb-0 ms-2" data-postid="<?php echo $post["post_id"]; ?>"
@@ -171,6 +173,7 @@
                       </div>
                       <?php if ($showingAdoptions): ?>
                         <?php if ($post['adopted']): ?>
+                          <!-- tag "adopted" -->
                           <button type="button" class="btn btn-primary" disabled>
                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
                               class="bi bi-check-lg me-2" viewBox="0 0 16 16">
@@ -180,6 +183,7 @@
                             Adopted
                           </button>
                         <?php else: ?>
+                          <!-- adoption request icon -->
                           <div class="adoption-icon" data-bs-toggle="modal" data-bs-target="#adoption-modal"
                             data-post-id="<?php echo $post["post_id"]; ?>"
                             data-owner="<?php echo $post["owner"]["username"]; ?>">
@@ -192,6 +196,7 @@
                           </div>
                         <?php endif; ?>
                       <?php endif; ?>
+                      <!-- icona del commento -->
                       <div class="comment-icon" data-bs-toggle="modal" data-bs-target="#comment-modal"
                         data-post-id="<?php echo $post["post_id"]; ?>"
                         data-post-owner="<?php echo $post["owner"]["username"]; ?>">
@@ -199,6 +204,7 @@
                           viewBox="0 0 16 16">
                           <path
                             d="M2.678 11.894a1 1 0 0 1 .287.801 11 11 0 0 1-.398 2c1.395-.323 2.247-.697 2.634-.893a1 1 0 0 1 .71-.074A8 8 0 0 0 8 14c3.996 0 7-2.807 7-6s-3.004-6-7-6-7 2.808-7 6c0 1.468.617 2.83 1.678 3.894m-.493 3.905a22 22 0 0 1-.713.129c-.2.032-.352-.176-.273-.362a10 10 0 0 0 .244-.637l.003-.01c.248-.72.45-1.548.524-2.319C.743 11.37 0 9.76 0 8c0-3.866 3.582-7 8-7s8 3.134 8 7-3.582 7-8 7a9 9 0 0 1-2.347-.306c-.52.263-1.639.742-3.468 1.105" />
+                            <text x="8" y="9" font-size="6" text-anchor="middle" alignment-baseline="middle"><?php echo count($post['comment']) ?></text>
                         </svg>
                       </div>
                     </div>
