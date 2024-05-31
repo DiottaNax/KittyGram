743524d6da3a62d61f8ffcff298683c377078d9f
Nax <federico.diotallevi@studio.unibo.it>
2024-05-31 20:50:27 +0200

    corrected fill tables order to avoid conflicts

--- db/populate.sql
+++ db/populate.sql
@@ -22,17 +22,30 @@ SET time_zone = "+00:00";
 --
 
 --
+-- Dump immagini di profilo
+--
+
+INSERT INTO `media` (`media_id`, `file_name`, `post_id`) VALUES
+(2, 'astroPic.jpg', NULL),
+(3, 'diottaPic.jpg', NULL),
+(4, 'gioPic.jpg', NULL),
+(5, 'agataPic.jpg', NULL),
+(6, 'marcoPic.jpg', NULL),
+(7, 'eugiPic.jpg', NULL),
+(8, 'volumePic.jpg', NULL)
+
+--
 -- Dump dei dati per la tabella `account`
 --
 
 INSERT INTO `account` (`user_id`, `username`, `email`, `password`, `salt`, `first_name`, `last_name`, `user_bio`, `profile_pic_id`, `city_id`) VALUES
-(1, 'diottanax', 'fd.diotallevi@gmail.com', 'b6df5d8385489b76c02cd195b1d525575138bcf66a9e561c804a4973cad6aef5807f0f7fd7527a6ca64e67ec6f16e44aad4e8df7508838da1e4169f88502bc66', '6ca2f9876217379c1c1c78bd94d5f31633da45ee0f0843caf433ba8d3f389755ac3077d111cc7ca915aedd4d1440c9f72ab377ad0c85242f6be4b573bb5ab4ed', 'Federico', 'Diotallevi', 'troppi gattini nella mia capa  😞 😞 😞', 7, NULL),
-(2, 'astrobaleno', 'astrobaleno@gmail.com', '0b629f4463b8c77d84ac0d41f123ffe454b6ac8f7a92dd0e9488cb87454771db1dbc1e3b05cf429213a179904229391125f5478cf59d0b50b58f663ae202f2a9', '4e221de901fafe36f75dbec1366de962a53b6dc8c6100abac1c8983e9e448b5994c7adb90cba11be003f20126034994a1be1adddba51f89a66a26b75a8c80f70', 'Javid', 'Ameri', '\"do not disturb\"', 2, NULL),
-(3, 'saint_g', 'saint138@gmail.com', '70c6e65e095f3bdceb39dfed51c2e138bb6e2501484b327a792be8b944f452430d991f371cbf2b4fb8c6f0d5b9abeb44a0914a932fda020d0d541cdbe07b1901', '9e4adcbf57452d8ed089627a2c0a4afff8d65cfa747cdb78adb2c131c28b4534cc0750775f58afef4464eb2433dedb3e5be40091a08f007571d3e54d61b80a59', 'Gioele', 'Santi', 'mi sono autoproclamato CEO di KittyGram >:)', 9, NULL),
-(4, 'aagatac', 'agatac@gmail.com', '775d7ea22c54f807c30d5743162ffaa2058b410816e8cadad95b761d840ff76a8b4924f2700e6e22ca499b8e2a9df66c20b8229c26ab5896fd424f5c0bcd5425', '8a7e65230345a5257f5f32406b7c2f55635e4536a42919b6f0850e038d9ecac21ac6e93f54372f2856055bba5c6250a2a7c77fcfe878e43ca711585ee1bf7ab5', 'Agata', 'Campolongo', 'Quando mi vidi\r\nNon c\'ero', 12, NULL),
-(5, 'silvi.marco', 'marcos@gmail.com', 'c15155ee965814445b3a1fbc2016d79d32d93f25473e0b4dd20db309bc20411915473ab8e161b722ca4aa22ade0125a1aaecb8e0a889749208568e54b3e4a91c', '6f481de821dffaaa0519f488723e957bdb9ff871497e002e608eb0847b150f801a9f1e070d4c946547b263e287af0310af5177468dbd8ce5c9a883faf31dd89e', 'Marco', 'Silvi', '📍  Fossombrone\r\n📚  Fisica, UniBo', 13, NULL),
-(6, 'volume_campus_cesena', 'volume@gmail.com', 'ae94d57d34f826ea183da9c854f67d63e9178a527df83cb3ec45d43ef01b509aee7c25310d2ea73b8a2cf66d1ae504a3f9c2989a600859f6e2de335a7ddad3bf', '5230f560b21d66ea4f722e71feac195d05aad3fc000e994477ce5475bd0f3ceeea3f65fe42d6eebb3ceaa173646da57f874931229a8640b2ebef47055aa7f2c3', 'VOLUME', 'CESENA', 'Spazio ※ Ascolto ※ Alimento\r\n\r\n\r\n📞 Prenota ora quello che vuoi dal menu: \r\n      32*******5\r\n\r\n🕒 Dal Lunedì al Venerdì: 8:30-18:00\r\n📖 Studio\r\n☕ Caffetteria\r\n🍹 Aperitivi\r\n\r\nwww.aidoru.org/spaces-volume.html', 15, NULL),
-(7, 'eugeniasantii', 'gegia@gmail.com', '041b61ec9da10547d90817c4cb6db8e168417bd97144819ea26dc26b812caa4a39e7e01aa92e2d9519982c3ca04bc5d300888a3bf1e6b5f8e5026995a4ad02f0', '9737aef01bf9d309e974edf6e30fc220066b5e49ecb5fe7f87329236383676e8b3b10bb19f12acc28949aea4e99281f02a5278e8993899d7c618c2b1557563ed', 'Eugenia', 'Santi', '📸 Instagram:  @eugeniasantii', 14, NULL);
+(1, 'diottanax', 'fd.diotallevi@gmail.com', 'b6df5d8385489b76c02cd195b1d525575138bcf66a9e561c804a4973cad6aef5807f0f7fd7527a6ca64e67ec6f16e44aad4e8df7508838da1e4169f88502bc66', '6ca2f9876217379c1c1c78bd94d5f31633da45ee0f0843caf433ba8d3f389755ac3077d111cc7ca915aedd4d1440c9f72ab377ad0c85242f6be4b573bb5ab4ed', 'Federico', 'Diotallevi', 'troppi gattini nella mia capa  😞 😞 😞', 7, 3),
+(2, 'astrobaleno', 'astrobaleno@gmail.com', '0b629f4463b8c77d84ac0d41f123ffe454b6ac8f7a92dd0e9488cb87454771db1dbc1e3b05cf429213a179904229391125f5478cf59d0b50b58f663ae202f2a9', '4e221de901fafe36f75dbec1366de962a53b6dc8c6100abac1c8983e9e448b5994c7adb90cba11be003f20126034994a1be1adddba51f89a66a26b75a8c80f70', 'Javid', 'Ameri', '\"do not disturb\"', 2, 2),
+(3, 'saint_g', 'saint138@gmail.com', '70c6e65e095f3bdceb39dfed51c2e138bb6e2501484b327a792be8b944f452430d991f371cbf2b4fb8c6f0d5b9abeb44a0914a932fda020d0d541cdbe07b1901', '9e4adcbf57452d8ed089627a2c0a4afff8d65cfa747cdb78adb2c131c28b4534cc0750775f58afef4464eb2433dedb3e5be40091a08f007571d3e54d61b80a59', 'Gioele', 'Santi', 'mi sono autoproclamato CEO di KittyGram >:)', 9, 4),
+(4, 'aagatac', 'agatac@gmail.com', '775d7ea22c54f807c30d5743162ffaa2058b410816e8cadad95b761d840ff76a8b4924f2700e6e22ca499b8e2a9df66c20b8229c26ab5896fd424f5c0bcd5425', '8a7e65230345a5257f5f32406b7c2f55635e4536a42919b6f0850e038d9ecac21ac6e93f54372f2856055bba5c6250a2a7c77fcfe878e43ca711585ee1bf7ab5', 'Agata', 'Campolongo', 'Quando mi vidi\r\nNon c\'ero', 12, 5),
+(5, 'silvi.marco', 'marcos@gmail.com', 'c15155ee965814445b3a1fbc2016d79d32d93f25473e0b4dd20db309bc20411915473ab8e161b722ca4aa22ade0125a1aaecb8e0a889749208568e54b3e4a91c', '6f481de821dffaaa0519f488723e957bdb9ff871497e002e608eb0847b150f801a9f1e070d4c946547b263e287af0310af5177468dbd8ce5c9a883faf31dd89e', 'Marco', 'Silvi', '📍  Fossombrone\r\n📚  Fisica, UniBo', 13, 6),
+(6, 'volume_campus_cesena', 'volume@gmail.com', 'ae94d57d34f826ea183da9c854f67d63e9178a527df83cb3ec45d43ef01b509aee7c25310d2ea73b8a2cf66d1ae504a3f9c2989a600859f6e2de335a7ddad3bf', '5230f560b21d66ea4f722e71feac195d05aad3fc000e994477ce5475bd0f3ceeea3f65fe42d6eebb3ceaa173646da57f874931229a8640b2ebef47055aa7f2c3', 'VOLUME', 'CESENA', 'Spazio ※ Ascolto ※ Alimento\r\n\r\n\r\n📞 Prenota ora quello che vuoi dal menu: \r\n      32*******5\r\n\r\n🕒 Dal Lunedì al Venerdì: 8:30-18:00\r\n📖 Studio\r\n☕ Caffetteria\r\n🍹 Aperitivi\r\n\r\nwww.aidoru.org/spaces-volume.html', 15, 8),
+(7, 'eugeniasantii', 'gegia@gmail.com', '041b61ec9da10547d90817c4cb6db8e168417bd97144819ea26dc26b812caa4a39e7e01aa92e2d9519982c3ca04bc5d300888a3bf1e6b5f8e5026995a4ad02f0', '9737aef01bf9d309e974edf6e30fc220066b5e49ecb5fe7f87329236383676e8b3b10bb19f12acc28949aea4e99281f02a5278e8993899d7c618c2b1557563ed', 'Eugenia', 'Santi', '📸 Instagram:  @eugeniasantii', 14, 7);
 
 --
 -- Dump dei dati per la tabella `follow`
@@ -125,21 +138,13 @@ INSERT INTO `comment` (`comment_id`, `post_id`, `user_id`, `message`, `date`) VA
 --
 
 INSERT INTO `media` (`media_id`, `file_name`, `post_id`) VALUES
-(1, 'default-pic.png', NULL),
-(2, 'astroPic.jpg', NULL),
-(3, 'gatto13.jpg', 1),
-(4, 'gatto15.jpg', 2),
-(5, 'gatto11.jpg', 3),
-(6, 'gatto2.jpg', 4),
-(7, 'diottaPic.jpg', NULL),
-(8, 'gatto1.jpg', 5),
-(9, 'gioPic.jpg', NULL),
-(10, 'cane-scodinzolante.gif', 6),
-(11, 'gatto7.jpg', 7),
-(12, 'agataPic.jpg', NULL),
-(13, 'marcoPic.jpg', NULL),
-(14, 'eugiPic.jpg', NULL),
-(15, 'volumePic.jpg', NULL),
+(9, 'gatto13.jpg', 1),
+(10, 'gatto15.jpg', 2),
+(11, 'gatto11.jpg', 3),
+(12, 'gatto2.jpg', 4),
+(13, 'gatto1.jpg', 5),
+(14, 'cane-scodinzolante.gif', 6),
+(15, 'gatto7.jpg', 7),
 (16, 'cucciolata.jpg', 8),
 (17, 'cucciolata2.jpg', 9),
 (18, 'gatto5.jpg', 10),
